743090e4caa15a5885d55ee5b7c141d047c09c7d
Renato Ruppel <heruppel2@gmail.com>
2021-12-19 14:47:13 -0300

    Added lint configuration

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: true,
+  printWidth: 110,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  endOfLine: 'crlf'
+};
