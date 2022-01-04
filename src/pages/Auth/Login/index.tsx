7b1c0a622a40bba84a03f0ad4dde7467149f5956
Renato Ruppel <heruppel2@gmail.com>
2022-01-04 19:23:15 -0300

    Added inputs and form to login page

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import { Button, Grid, Typography } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import { useStyles } from './styles';
+import { FormProvider, useForm } from 'react-hook-form';
+import Input from '../../../components/Input';
+
+interface FormCredentials {
+  email: string;
+  password: string;
+}
 
 const Login = (): JSX.Element => {
   const classes = useStyles();
+  const formMethods = useForm<FormCredentials>();
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = () => {
+    console.log('SUBMIT');
+  };
 
   return (
     <Grid container className={classes.container}>
@@ -14,11 +27,28 @@ const Login = (): JSX.Element => {
         </Typography>
       </Grid>
       <Grid className={classes.right} item xs={6}>
-        <form className={classes.formContainer}>
+        <form className={classes.formContainer} onSubmit={handleSubmit(onSubmit)}>
           <Typography color='secondary' className={classes.header} variant='h5'>
             Insira suas credenciais
           </Typography>
-          {/* to do form */}
+          <FormProvider {...formMethods}>
+            <Input
+              variant='outlined'
+              name='email'
+              label='E-mail'
+              required
+              givenError='Insira seu endereço de e-mail'
+              defaultValue=''
+            />
+            <Input
+              variant='outlined'
+              name='password'
+              label='Senha'
+              type='password'
+              required
+              givenError='Insira sua senha'
+            />
+          </FormProvider>
           <div className={classes.bottomContainer}>
             <Button className={classes.button} type='submit'>
               Entrar
