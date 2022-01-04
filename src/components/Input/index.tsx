0f0561a90d0901eeac1a5966246ddb0b9343b78e
Renato Ruppel <heruppel2@gmail.com>
2022-01-04 19:07:53 -0300

    Completed input component and added new route for redirect

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { TextField, TextFieldProps } from '@material-ui/core';
+import { useFormContext } from 'react-hook-form';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const Input = () => {};
+type InputProps = {
+  name: string;
+  givenError?: string;
+  customClass?: string;
+} & TextFieldProps;
+
+export const Input = ({ name, givenError, customClass, required, ...rest }: InputProps): JSX.Element => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <TextField
+      className={customClass}
+      helperText={errors[name] && givenError}
+      {...register(name, { required: { value: required as boolean, message: givenError as string } })}
+      {...rest}
+    />
+  );
+};
 
 export default Input;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import Home from '../pages/Home';
 const AppRoutes = (): JSX.Element => {
   return (
     <Routes>
+      <Route path='/' element={<Navigate to='/home' />} />
       <Route path='/home' element={<Navigate to='/home/posts' />} />
       <Route path='/auth' element={<Navigate to='/auth/login' />} />
       <Route path='/home/*' element={<Home />} />
