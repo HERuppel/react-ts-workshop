e2365658980d83d51ec0dd60a9daa3dcf578dcfd
Renato Ruppel <heruppel2@gmail.com>
2022-01-04 19:31:01 -0300

    Added register forms

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -2,19 +2,59 @@ import React from 'react';
 import { Button, Typography } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
 import { useStyles } from './styles';
+import { FormProvider, useForm } from 'react-hook-form';
+import Input from '../../../components/Input';
+
+interface SignUpProps {
+  email: string;
+  name: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register = (): JSX.Element => {
   const classes = useStyles();
+  const formMethods = useForm<SignUpProps>();
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = () => {
+    console.log('register');
+  };
 
   return (
     <div className={classes.container}>
-      <form className={classes.formContainer}>
+      <form className={classes.formContainer} onSubmit={handleSubmit(onSubmit)}>
         <div className={classes.header}>
           <Typography color='secondary' variant='h5'>
             Insira suas informações para o registro
           </Typography>
         </div>
-        {/* to do form*/}
+        <FormProvider {...formMethods}>
+          <Input
+            variant='outlined'
+            name='email'
+            label='E-mail'
+            required
+            givenError='Insira um endereço de e-mail'
+          />
+          <Input variant='outlined' name='name' label='Nome' required givenError='Insira seu nome' />
+          <Input
+            variant='outlined'
+            name='password'
+            label='Senha'
+            type='password'
+            required
+            givenError='Insira uma senha'
+          />
+          <Input
+            variant='outlined'
+            name='confirmPassword'
+            label='Confirme a senha'
+            type='password'
+            required
+            givenError='Insira a senha novamente'
+          />
+        </FormProvider>
         <div className={classes.bottomContainer}>
           <Button className={classes.button} type='submit'>
             Cadastrar
