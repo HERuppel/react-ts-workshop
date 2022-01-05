d6025fbcc54d4ba997313750c05ce47a267eb887
Renato Ruppel <heruppel2@gmail.com>
2022-01-05 18:47:23 -0300

    added user email and name to user page

--- src/pages/Home/User/index.tsx
+++ src/pages/Home/User/index.tsx
@@ -3,9 +3,11 @@ import { useStyles } from './styles';
 
 import tommy from '../../../assets/tommy.png';
 import { Typography } from '@material-ui/core';
+import { useAuth } from '../../../hooks/Auth';
 
 const User = (): JSX.Element => {
   const classes = useStyles();
+  const { user } = useAuth();
 
   return (
     <div className={classes.container}>
@@ -13,10 +15,10 @@ const User = (): JSX.Element => {
         <img className={classes.img} src={tommy} alt='profile' />
         <div className={classes.userInfos}>
           <Typography variant='h5' color='textPrimary'>
-            Nome
+            {user.name}
           </Typography>
           <Typography variant='h5' color='textSecondary'>
-            E-mail
+            {user.email}
           </Typography>
         </div>
       </div>
