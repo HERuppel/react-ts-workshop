07a7433cc49eac7a36caefe28fc3354687bb9160
Renato Ruppel <heruppel2@gmail.com>
2022-01-05 18:22:26 -0300

    Auth context completed

--- src/hooks/Auth/index.tsx
+++ src/hooks/Auth/index.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
 import { User } from '../../@types/User';
 import { Response } from '../../@types/Response';
-import { api } from '../../services';
+import { api } from '../../services/api';
 
 interface AuthCredentials {
   email: string;
@@ -28,11 +28,33 @@ interface AuthContextData {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<User>({} as User);
+  const [user, setUser] = useState<User>(() => {
+    const storagedUser = localStorage.getItem('@krud:user');
+    const storagedToken = localStorage.getItem('@krud:token');
 
-  const signIn = useCallback(async () => {}, []);
+    if (storagedUser && storagedToken) {
+      api.defaults.headers.common['authorization'] = `Bearer ${storagedToken}`;
+      return JSON.parse(storagedUser);
+    }
+    return {} as User;
+  });
 
-  const signUp = useCallback(async () => {}, []);
+  const signIn = useCallback(async (credentials: AuthCredentials) => {
+    const { data }: { data: Response<LoginResponse> } = await api.post('auth/login', credentials);
+
+    localStorage.setItem('@krud:user', JSON.stringify(data.content?.user));
+    localStorage.setItem('@krud:token', data.content?.token as string);
+
+    api.defaults.headers.common['authorization'] = `Bearer ${data.content?.token}`;
+
+    setUser(data.content?.user as User);
+  }, []);
+
+  const signUp = useCallback(async (userData: SignUpData): Promise<Response<User>> => {
+    const { data }: { data: Response<User> } = await api.post('user/create', userData);
+
+    return data;
+  }, []);
 
   return (
     <AuthContext.Provider
