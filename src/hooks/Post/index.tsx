b571f512c06de9e9c55eb69f6238e99cdaf48984
Renato Ruppel <heruppel2@gmail.com>
2022-01-05 19:03:58 -0300

    added post context and provider

---
+++ src/hooks/Post/index.tsx
@@ -0,0 +1,64 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import { Post } from '../../@types/Post';
+import { Response } from '../../@types/Response';
+import { api } from '../../services/api';
+
+interface PostContextData {
+  posts: Post[];
+  addPost: (newPost: Post) => Promise<Response<Post>>;
+  fetchPosts: () => Promise<void>;
+  fetchPostsByUser: (userId: string) => Promise<Response<Post[]>>;
+}
+
+const PostContext = createContext<PostContextData>({} as PostContextData);
+
+export const PostProvider: React.FC = ({ children }) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const addPost = useCallback(async (newPost: Post): Promise<Response<Post>> => {
+    const { data }: { data: Response<Post> } = await api.post('post/create', newPost);
+
+    setPosts(prev => {
+      const newPosts = [data.content as Post, ...prev];
+
+      return newPosts;
+    });
+
+    return data;
+  }, []);
+
+  const fetchPosts = useCallback(async (): Promise<void> => {
+    const { data }: { data: Response<Post[]> } = await api.get('post/read');
+
+    setPosts(data.content as Post[]);
+  }, []);
+
+  const fetchPostsByUser = useCallback(async (userId: string): Promise<Response<Post[]>> => {
+    const { data }: { data: Response<Post[]> } = await api.get(`post/read/${userId}`);
+
+    return data;
+  }, []);
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts,
+        addPost,
+        fetchPosts,
+        fetchPostsByUser,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+};
+
+export const usePost = (): PostContextData => {
+  const context = useContext(PostContext);
+
+  if (!context) {
+    throw new Error('Este recurso deve ser utilizado dentro de seu provider');
+  }
+
+  return context;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from '../hooks/Auth';
+import { PostProvider } from '../hooks/Post';
 import Auth from '../pages/Auth';
 import Home from '../pages/Home';
 
@@ -11,7 +12,14 @@ const AppRoutes = (): JSX.Element => {
         <Route path='/' element={<Navigate to='/home' />} />
         <Route path='/home' element={<Navigate to='/home/posts' />} />
         <Route path='/auth' element={<Navigate to='/auth/login' />} />
-        <Route path='/home/*' element={<Home />} />
+        <Route
+          path='/home/*'
+          element={
+            <PostProvider>
+              <Home />
+            </PostProvider>
+          }
+        />
         <Route path='/auth/*' element={<Auth />} />
       </Routes>
     </AuthProvider>
