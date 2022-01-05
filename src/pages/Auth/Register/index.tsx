ea5acb8869f8e72a5efb361a8097c0a38673b3a5
Renato Ruppel <heruppel2@gmail.com>
2022-01-05 18:37:41 -0300

    added auth provider and created register function in the register page

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Typography } from '@material-ui/core';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useStyles } from './styles';
 import { FormProvider, useForm } from 'react-hook-form';
 import Input from '../../../components/Input';
+import { useAuth } from '../../../hooks/Auth';
+import { showAlert } from '../../../utils/showAlert';
+import Loading from '../../../components/Loading';
 
 interface SignUpProps {
   email: string;
@@ -16,9 +19,45 @@ const Register = (): JSX.Element => {
   const classes = useStyles();
   const formMethods = useForm<SignUpProps>();
   const { handleSubmit } = formMethods;
+  const { signUp } = useAuth();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const onSubmit = () => {
-    console.log('register');
+  const onSubmit = async (data: SignUpProps): Promise<void> => {
+    const { confirmPassword, ...userData } = data;
+
+    if (confirmPassword !== userData.password) {
+      showAlert({
+        title: 'Atenção!',
+        icon: 'warning',
+        text: 'As senhas precisam ser iguais!',
+      });
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const res = await signUp(userData);
+
+      showAlert({
+        title: 'Sucesso!',
+        icon: 'success',
+        text: res.message,
+      }).then(result => {
+        if (result.isConfirmed) {
+          navigate('/auth/login', { state: { email: res.content?.email } });
+        }
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      showAlert({
+        title: 'Ops...',
+        icon: 'error',
+        text: err.response.data.message,
+      });
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -57,7 +96,7 @@ const Register = (): JSX.Element => {
         </FormProvider>
         <div className={classes.bottomContainer}>
           <Button className={classes.button} type='submit'>
-            Cadastrar
+            {loading ? <Loading loadingSize={16} /> : 'Cadastrar'}
           </Button>
           <NavLink className={classes.signIn} to='/auth/login'>
             <Typography variant='h6'>Já possuo uma conta</Typography>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from '../hooks/Auth';
 import Auth from '../pages/Auth';
 import Home from '../pages/Home';
 
 const AppRoutes = (): JSX.Element => {
   return (
-    <Routes>
-      <Route path='/' element={<Navigate to='/home' />} />
-      <Route path='/home' element={<Navigate to='/home/posts' />} />
-      <Route path='/auth' element={<Navigate to='/auth/login' />} />
-      <Route path='/home/*' element={<Home />} />
-      <Route path='/auth/*' element={<Auth />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<Navigate to='/home/posts' />} />
+        <Route path='/auth' element={<Navigate to='/auth/login' />} />
+        <Route path='/home/*' element={<Home />} />
+        <Route path='/auth/*' element={<Auth />} />
+      </Routes>
+    </AuthProvider>
   );
 };
 export default AppRoutes;
