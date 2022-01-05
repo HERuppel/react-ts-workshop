bf6225f1ab96985fe42594c0bc3afa9a6f39d7f4
Renato Ruppel <heruppel2@gmail.com>
2022-01-05 19:32:38 -0300

    Completed user profile with posts

--- src/pages/Home/User/index.tsx
+++ src/pages/Home/User/index.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStyles } from './styles';
 
 import tommy from '../../../assets/tommy.png';
 import { Typography } from '@material-ui/core';
 import { useAuth } from '../../../hooks/Auth';
+import { showAlert } from '../../../utils/showAlert';
+import { Post } from '../../../@types/Post';
+import { usePost } from '../../../hooks/Post';
+import Loading from '../../../components/Loading';
+import PostCard from '../../../components/PostCard';
 
 const User = (): JSX.Element => {
   const classes = useStyles();
   const { user } = useAuth();
+  const { fetchPostsByUser } = usePost();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [myPosts, setMyPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetchPostsByUser(user.id as string);
+
+        setMyPosts(res.content as Post[]);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        showAlert({
+          title: 'Ops...',
+          icon: 'error',
+          text: err.response.data.message,
+        });
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [fetchPostsByUser, user]);
 
   return (
     <div className={classes.container}>
@@ -26,7 +53,13 @@ const User = (): JSX.Element => {
         <Typography variant='h3' color='primary'>
           Meus Posts
         </Typography>
-        {/* List user posts */}
+        {loading ? (
+          <div className={classes.loadingContainer}>
+            <Loading loadingSize={50} />
+          </div>
+        ) : (
+          myPosts.map(post => <PostCard key={post.id} post={post} />)
+        )}
       </div>
     </div>
   );
